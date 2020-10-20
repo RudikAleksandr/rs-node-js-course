295ed0525cc62ef68fc4f813ca32122f1eb6fa18
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2020-10-20 23:23:47 +0300

    feat: add logger module

---
+++ src/common/logger.js
@@ -0,0 +1,25 @@
+const { createLogger, transports } = require('winston');
+
+const logger = createLogger({
+  transports: [
+    new transports.File({
+      level: 'http',
+      filename: './src/logs/http.log'
+    }),
+    new transports.File({
+      level: 'error',
+      filename: './src/logs/error.log'
+    })
+  ]
+});
+
+const logRequest = (req, res, next) => {
+  const { method, url, body } = req;
+  logger.http(`${method} ${url} body: ${JSON.stringify(body)}`);
+  next();
+};
+
+module.exports = {
+  logger,
+  logRequest
+};
