51d68cac3fa8c4368ac1a1eda0b534f5b9834911
RudikAleksandr <aleksandr.rudin.98@gmail.com>
2020-10-20 23:25:22 +0300

    feat: add handling errors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
+const { logger, logRequest } = require('./common/logger');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
@@ -9,8 +11,16 @@ const taskRouter = require('./resources/tasks/task.router');
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
-app.use(express.json());
+process.on('uncaughtException', err => {
+  logger.error(`captured error: ${err.message}`);
+});
 
+process.on('unhandledRejection', reason => {
+  logger.error(`Unhandled rejection detected: ${reason.message}`);
+});
+
+app.use(express.json());
+app.use(logRequest);
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/', (req, res, next) => {
@@ -23,5 +33,11 @@ app.use('/', (req, res, next) => {
 
 app.use('/users', userRouter);
 app.use('/boards', [boardRouter, taskRouter]);
+
+app.use((err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
+  next(err);
+});
 
 module.exports = app;
